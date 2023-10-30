ee72851a09f70e38ac7bed90a2a13764dabc3f72
Zach Rizzo <85094894+zachrizzo@users.noreply.github.com>
2023-10-30 16:24:47 +0100

    node

--- node_server/app.js
+++ node_server/app.js
@@ -23,7 +23,7 @@ app.use("/api/payment-history", paymentHistoryRoutes);
 app.use("/api/cars", carRoutes);
 app.use("/api/car-charge-history", carChargeHistoryRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`listening on port ${port}...`);
 });
